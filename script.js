805b2097f2a79c07aba0aff631af9cece52644a1
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-07-07 02:40:34 -0500

    created password validation, and criteria for the password using const variables and confirm

--- script.js
+++ script.js
@@ -4,7 +4,23 @@ var generateBtn = document.querySelector("#generate");
 // This line of code is used to generate a password when the button is clicked.
 document.getElementById("generate").addEventListener("click", generatePassword);
 
+function generatePassword() {
+  const length = prompt("Enter desired password length between 8 and 128 characters.");
 
+  // This line of code is used to validate the password length.
+  if (!length || isNaN(length) || length < 8 || length > 128) {
+    alert("Invalid password length! Please enter a number between 8 and 128.");
+    return;
+  }
+
+  // This line of code is used to confirm the password criteria.
+  const includeLowercase = confirm("Do you want to include lowercase characters?");
+  const includeUppercase = confirm("Do you want to include uppercase characters?");
+  const includeNumeric = confirm("Do you want to include numeric characters?");
+  const includeSpecial = confirm("Do you want to include special characters?");
+
+  
+}
 
 
 
